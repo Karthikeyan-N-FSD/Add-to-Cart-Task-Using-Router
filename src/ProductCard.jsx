86d3302f339b150c8a9edfb2feddc18caea48acf
Karthikeyan-N-FSD <karthikeyan89@gmail.com>
2025-02-18 14:51:50 +0530

    PC Design change

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -3,23 +3,23 @@ import { Link } from 'react-router'
 
 export function ProductCard(props) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 flex flex-col justify-between hover:shadow-xl">
-      <Link to={`/product/${props.product.id}`}><img className="w-full h-48 object-contain rounded-md mb-4" src={props.product.image} alt={props.product.title} /></Link>
+    <div className="bg-white rounded-lg shadow-md p-4 flex flex-col justify-between hover:shadow-lg transition-shadow duration-150 ease-in">
+      <Link to={`/product/${props.product.id}`}><img className="w-full h-48 object-contain rounded-lg mb-3" src={props.product.image} alt={props.product.title} /></Link>
       <div>
         <Link to={`/product/${props.product.id}`}>
-          <h3 className="text-lg font-semibold line-clamp-2" title={props.product.title}>{props.product.title}</h3>
-          <div className='flex justify-between'>
-            <div className="text-gray-700">₹ {props.product.price}</div>
+          <h3 className="text-lg font-semibold line-clamp-2 mb-1" title={props.product.title}>{props.product.title}</h3>
+          <div className='flex justify-between items-center'>
+            <div className="text-gray-700 font-medium">₹ {props.product.price}</div>
             <div className="flex items-center gap-2">
-              <span>({props.product.rating.count})</span>
-              <span className={`flex items-center px-1 rounded text-white ${getRatingColor(props.product.rating.rate)}`}>
+              <span className="text-gray-500">({props.product.rating.count})</span>
+              <span className={`flex items-center px-2 py-0.5 rounded-full text-white ${getRatingColor(props.product.rating.rate)}`}>
                 {props.product.rating.rate}
-                <StarIcon className="size-5 text-white" />
+                <StarIcon className="size-4 text-white" />
               </span>
             </div>
           </div>
         </Link>
-        <button onClick={() => { props.addToCart(props.product) }} className='w-full mt-2 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Add to cart</button>
+        <button onClick={() => { props.addToCart(props.product) }} className='w-full mt-3 bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-800 transition-colors duration-150 ease-in'>Add to cart</button>
       </div>
     </div>
   )
@@ -29,8 +29,8 @@ function getRatingColor(rating) {
   if (rating >= 4) {
     return 'bg-green-600';
   } else if (rating >= 2) {
-    return 'bg-yellow-600';
+    return 'bg-yellow-500';
   } else {
-    return 'bg-red-600';
+    return 'bg-red-500';
   }
 }
